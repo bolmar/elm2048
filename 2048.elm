a9a3df9062f96e887735c6903c4a55851c7ea1e8
Samuel Strand <samuel.strand@klarna.com>
2014-04-03 12:36:25 +0200

    Clean up/refactor

--- 2048.elm
+++ 2048.elm
@@ -3,16 +3,15 @@ import Random
 import Either (..)
 
 startgrid : [[Int]]
-startgrid = addnew 5 <| addnew 10 <| split 4 <| repeat 16 0
--- split 4 <| repeat 16 0
+startgrid = addnew' 5 2 <| addnew' 10 2 <| split 4 <| repeat 16 0
 
-main = flow' . show' <~ foldp folder startgrid (input arrows)
+main = flow' . show' <~ foldp step startgrid (input arrows)
 
--- layout
-flow' : [[Element]] -> Element   
+-- layout elements
+flow' : [[Element]] -> Element
 flow' = flow down . map (flow right)
 
--- "print"
+-- translate grid to graphical elements
 show' : [[Int]] -> [[Element]]
 show' = map (map box)
 
@@ -20,28 +19,37 @@ box : Int -> Element
 box n = colour n <| container 50 50 middle <| plainText <| if n == 0 then " " else show n
 
 -- act on keypress and maybe add a new tile
-folder : ((Int, Int), Float) -> [[Int]] -> [[Int]]
-folder (keypress, randseed) grid =
- action keypress grid |> either id (addnew' randseed)
+step : ((Int, Int), (Float, Float)) -> [[Int]] -> [[Int]]
+step (keypress, randseed) grid =
+ action keypress grid |> either id (addnew randseed)
 
+-- select merge direction based on key input
+action : (Int, Int) -> [[Int]] -> Either [[Int]] [[Int]]
+action key =
+ case key of
+  ( -1,  0) -> rows merge                         -- left
+  (  0,  1) -> cols merge                         -- up   
+  (  1,  0) -> rows <| on merge reverse . reverse -- right
+  (  0, -1) -> cols <| on merge reverse . reverse -- down
+  _         -> Left . id
 
 merge : [Int] -> Either [Int] [Int]
 merge l =
- if all (\e -> e == 0) <| tail l
+ if all ((==) 0) <| tail l
   then Left l
   else
    case l of
-    0 :: t      -> right' <| merge <| t ++ [0]
-    a :: 0 :: t -> right' <| merge <| a :: t ++ [0]
-    a :: b :: t -> if a == b then right' . on merge ((::) (a + a)) <| t ++ [0]
+    0 :: t      ->                right' . merge                   <|      t ++ [0]
+    a :: 0 :: t ->                right' . merge                   <| a :: t ++ [0]
+    a :: b :: t -> if a == b then right' . on merge ((::) (a + a)) <|      t ++ [0]
                              else          on merge ((::) a)       <| b :: t
 
--- grid changed
+-- signal grid change so a new tile will spawn
 right' = either Right Right
 
 -- merge left/right
 rows : ([Int] -> Either [Int] [Int]) -> [[Int]] -> Either [[Int]] [[Int]]
-rows mergef = reduce . map mergef 
+rows mergef = reduce . map mergef
 
 -- merge up/down
 cols : ([Int] -> Either [Int] [Int]) -> [[Int]] -> Either [[Int]] [[Int]]
@@ -50,7 +58,6 @@ cols mergef = on (rows mergef) transpose . transpose
 -- basically lift . into either
 on : (a -> Either b b) -> (b -> c) -> a -> Either c c
 on f g = (either (Left . g) (Right . g)) . f
-
 -- combine?
 reduce : [Either a a] -> Either [a] [a]
 reduce l = if any isRight l then Right <| map strip l
@@ -59,58 +66,49 @@ reduce l = if any isRight l then Right <| map strip l
 -- escape from eitherland
 strip = either id id
 
-transpose : [[Int]] -> [[Int]]
-transpose g =
- case g of
-  []      -> []
-  [] :: t -> transpose t
-  _       -> map head g :: (transpose <| map tail g)
+addnew : (Float, Float) -> [[Int]] -> [[Int]]
+addnew (rnd1, rnd2) grid =
+ let index = floor <| (*) rnd1 <| toFloat <| count0s grid
+ in addnew' rnd2 index grid
 
--- select merge direction based on key input
-action : (Int, Int) -> [[Int]] -> Either [[Int]] [[Int]]
-action key =
- case key of
-  ( -1,  0) -> rows merge                         -- left
-  (  0,  1) -> cols merge                         -- up
-  (  1,  0) -> rows <| on merge reverse . reverse -- right
-  (  0, -1) -> cols <| on merge reverse . reverse -- down
-  _         -> Left . id
+addnew' : Float -> Int -> [[Int]] -> [[Int]]
+addnew' rnd n g = split 4 <| setnth0 n (starttile rnd) <| concat g
+
+starttile : Float -> Int
+starttile rnd = if rnd > 0.92 then 2 else 1
 
 count0s : [[Int]] -> Int
-count0s = length . (filter (\x -> x == 0)) . concat
-
-select0 : Float -> [[Int]] -> Int
-select0 fl g = round <| fl * (toFloat <| (count0s g) - 1)
-
-addnew' : Float -> [[Int]] -> [[Int]]
-addnew' randseed grid =
- let index = select0 randseed grid
-     x     = if randseed > 0.92 then 4 else 2
- in split 4 <| setnth index 0 x <| concat grid
-
-addnew : Int -> [[Int]] -> [[Int]]
-addnew n g = split 4 <| setnth n 0 2 <| concat g
+count0s = length . (filter ((==) 0)) . concat
 
 split : Int -> [Int] -> [[Int]]
-split n l = if length l <= n then [l] else take n l :: (split n <| drop n l)
+split n l =
+ if length l <= n
+ then [l]
+ else take n l :: (split n <| drop n l)
 
-setnth n a b (x::xs) =
- if n == 0
- then if x == a then b :: xs else x :: (setnth n a b xs)
- else x :: (setnth (n - 1) a b xs)
+setnth0 : Int -> Int -> [Int] -> [Int]
+setnth0 n a (x::xs) =
+ if | n == 0 && x == 0 -> a :: xs
+    | n == 0 -> x :: (setnth0 n a xs)
+    | True   -> x :: (setnth0 (n - 1) a xs)
 
-input : Signal {x:Int, y:Int} -> Signal ((Int, Int), Float)
-input keys = (,) <~ lift (\{x, y} -> (x, y)) keys ~ Random.float keys
+input : Signal {x:Int, y:Int} -> Signal ((Int, Int), (Float, Float))
+input keys = (,) <~ lift (\{x, y} -> (x, y)) keys ~ randgen keys
 
 -- drop key up signals 
 arrows : Signal {x:Int, y:Int}
 arrows = dropIf (\{x,y} -> (x,y) == (0,0)) {x=0,y=1} Keyboard.arrows
 
+randgen : Signal a -> Signal (Float, Float)
+randgen sig = (\[f1,f2] -> (f1,f2)) <~ (Random.floatList <| always 2 <~ sig)
+
+-- colour element with nth colour
 colour : Int -> Element -> Element
-colour n = color <| head <| drop (logBase 2 n) colours
+colour n = color <| head <| drop (logBase 2 <| n + 1) colours
 
 colours =
   [ grey
+  , darkGrey
   , lightBlue
   , lightGreen
   , lightYellow
@@ -124,3 +122,10 @@ colours =
   , darkRed
   , darkPurple
   ] ++ repeat 10 grey
+
+transpose : [[Int]] -> [[Int]]
+transpose g =
+ case g of
+  []      -> []
+  [] :: t -> transpose t
+  _       -> map head g :: (transpose <| map tail g)
